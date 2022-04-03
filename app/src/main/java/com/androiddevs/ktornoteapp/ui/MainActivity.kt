db1fda25ee12b7cc5d503647064bbc63b5090ebf
Uthman Ayinde <uthmanayinde6@gmail.com>
2022-04-03 10:07:30 +0100

    added login and logout implementation

--- app/src/main/java/com/androiddevs/ktornoteapp/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/MainActivity.kt
@@ -3,10 +3,14 @@ package com.androiddevs.ktornoteapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.androiddevs.ktornoteapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
